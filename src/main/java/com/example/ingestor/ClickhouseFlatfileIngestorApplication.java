567dc784c949b1ba9f1e545c3018998c354a6d58
jitu7033 <jraj41797@gmail.com>
2025-04-14 16:15:15 +0530

    project setup

--- src/main/java/com/example/ingestor/ClickhouseFlatfileIngestorApplication.java
+++ src/main/java/com/example/ingestor/ClickhouseFlatfileIngestorApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClickhouseFlatfileIngestorApplication {
 	public static void main(String[] args) {
+		System.out.println("project setUP");
 		SpringApplication.run(ClickhouseFlatfileIngestorApplication.class, args);
 	}
 
