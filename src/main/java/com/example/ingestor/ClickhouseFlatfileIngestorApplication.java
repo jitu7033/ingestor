800e21b826ea77a1d5c4e6e0113cc4ab79fdcafa
jitu7033 <jraj41797@gmail.com>
2025-04-14 16:08:32 +0530

    project setup

---
+++ src/main/java/com/example/ingestor/ClickhouseFlatfileIngestorApplication.java
@@ -0,0 +1,12 @@
+package com.example.ingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickhouseFlatfileIngestorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ClickhouseFlatfileIngestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ingestor/ClickhouseFlatfileIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClickhouseFlatfileIngestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
